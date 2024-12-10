002d21fae5f17c418cacaae361569fba94e5bba2
Sawal <devkotasawal@yahoo.com>
2024-12-11 00:37:27 +0100

    reviewComment first draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReviewComment from './ReviewComment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+      <h1 style={{ textAlign: 'center' }}>Human Evaluation Interface</h1>
+      <ReviewComment />
     </div>
   );
 }
